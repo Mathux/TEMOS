3bbb460b8a27006a3159d9389441ed3591be5ef3
Mathis Petrovich <mathis.petrovich@enpc.fr>
2022-08-30 06:28:12 +0200

    fix path with gender

--- evaluate.py
+++ evaluate.py
@@ -104,7 +104,7 @@ def evaluate(cfg: DictConfig) -> None:
                                                           jointstype=cfg.jointstype)
     split = cfg.split
 
-    path = get_path(model_samples, cfg.split, onesample, cfg.mean, cfg.fact)
+    path = get_path(model_samples, cfg.gender, cfg.split, onesample, cfg.mean, cfg.fact)
     file_path = f"amass_metrics_{split}" if amass else f"metrics_{split}"
 
     save_paths = get_metric_paths(model_samples, amass, cfg.split, onesample, cfg.mean, cfg.fact)
